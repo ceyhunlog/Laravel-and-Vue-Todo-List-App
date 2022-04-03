d3958ad096ffb21b2667fb38dac6fb335ead9642
ceyhunlog <ceyhunbcelik.log@gmail.com>
2022-04-03 22:34:53 +0300

    setup vue

---
+++ resources/js/components/app.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Vue</h1>
+</template>
+
+<script>
+    export default { // 27.37
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
